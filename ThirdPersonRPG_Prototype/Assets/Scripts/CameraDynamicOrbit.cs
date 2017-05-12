059311827259e6924adc6729c8fa6e28eee4a4be
Unknown <gordon.lee.6170@gmail.com>
2017-05-12 10:53:19 -0400

    Ready to do multiplayer

--- ThirdPersonRPG_Prototype/Assets/Scripts/CameraDynamicOrbit.cs
+++ ThirdPersonRPG_Prototype/Assets/Scripts/CameraDynamicOrbit.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CameraDynamicOrbit : MonoBehaviour {
     public Transform followingTarget;
-    public float cameraDistance = 3;
     public float cameraMinDistance = 0.5f;
     public float cameraMaxDistance = 3;
     public float cameraRotationSpeed = 120;
@@ -12,7 +11,8 @@ public class CameraDynamicOrbit : MonoBehaviour {
     public float verticalMinAngle = -20;
     public float verticalMaxAngle = 80;
 
-    private float _cameraOffset = 1;
+    private float _cameraDistance = 3;
+    private float _cameraVerticalOffset = 1.65f;
     private Transform _camTrans;
     private Vector3 _veloctity = Vector3.zero;
 
@@ -24,7 +24,7 @@ public class CameraDynamicOrbit : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        
+        this.transform.position = new Vector3(0, _cameraVerticalOffset, 0);
     }
 	
 	// Update is called once per frame
@@ -35,33 +35,31 @@ public class CameraDynamicOrbit : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        cameraDistance = cameraMaxDistance;
+        _cameraDistance = cameraMaxDistance;
 
         if (Physics.Raycast(this.transform.position, -this.transform.forward, out _hit)) {
             if(_hit.collider.gameObject.layer == 8) {
                 if (_hit.distance < cameraMaxDistance) {
-                    cameraDistance = _hit.distance - 0.01f;
+                    _cameraDistance = _hit.distance - 0.5f;
                 }
-
-                Debug.Log(_hit.distance);
             }
         }
     }
 
     private void CameraRotate() {
-        this.transform.Rotate(Input.GetAxis("Mouse Y") * cameraRotationSpeed * Time.deltaTime, Input.GetAxis("Mouse X") * cameraRotationSpeed * Time.deltaTime, 0);
+        this.transform.Rotate(Input.GetAxis("CamVertical") * cameraRotationSpeed * Time.deltaTime, Input.GetAxis("CamHorizontal") * cameraRotationSpeed * Time.deltaTime, 0);
         this.transform.localEulerAngles = new Vector3(AngleClamp(this.transform.localEulerAngles.x, verticalMinAngle, verticalMaxAngle), this.transform.localEulerAngles.y, 0);
     }
 
     private void CameraFollow() {
         if (followingTarget != null) {
-            this.transform.position = Vector3.SmoothDamp(this.transform.position, new Vector3(followingTarget.position.x, followingTarget.position.y + Mathf.Clamp(_cameraOffset / cameraDistance, 1.5f, 1.7f), followingTarget.position.z), ref _veloctity, camDampSpeed);
+            this.transform.position = Vector3.SmoothDamp(this.transform.position, new Vector3(followingTarget.position.x, followingTarget.position.y + _cameraVerticalOffset, followingTarget.position.z), ref _veloctity, camDampSpeed);
         }
     }
 
     private void DynamicCameraDistance() {
-        cameraDistance = Mathf.Clamp(cameraDistance, cameraMinDistance, cameraMaxDistance);
-        _camTrans.localPosition = Vector3.Lerp(_camTrans.localPosition, new Vector3(0, 0, -cameraDistance), 30 * Time.deltaTime);
+        _cameraDistance = Mathf.Clamp(_cameraDistance, cameraMinDistance, cameraMaxDistance);
+        _camTrans.localPosition = Vector3.Lerp(_camTrans.localPosition, new Vector3(0, 0, -_cameraDistance), 30 * Time.deltaTime);
     }
 
     private float AngleClamp(float _angle, float _min, float _max) {
---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColor : MonoBehaviour {
+
+    public Color32 bodyColor = new Color32(255, 255, 255, 255);
+
+    private Renderer _renderer;
+
+	// Use this for initialization
+	void Start () {
+        _renderer = this.transform.FindChild("RotationPivot/HumanBody").GetComponent<Renderer>();
+        _renderer.materials[1].SetColor("_Color", bodyColor);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
