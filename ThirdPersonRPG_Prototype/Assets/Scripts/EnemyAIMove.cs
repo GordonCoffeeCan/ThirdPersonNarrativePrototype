243ced09baa1ed8c6c5aa6580ef537a681070517
Mostafa Haque <93haque@gmail.com>
2017-08-07 14:36:35 -0400

    Singleplayer Nav Mesh working

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/EnemyAIMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAIMove : MonoBehaviour {
+
+    public Transform target;
+    public Transform self;
+    private NavMeshAgent navigator;
+    // Use this for initialization
+    void Start()
+    {
+        self = this.transform;
+        navigator = this.gameObject.GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GameObject.FindGameObjectWithTag("SinglePlayer") != null)
+        {
+            target = GameObject.FindGameObjectWithTag("SinglePlayer").transform;
+
+        }
+        else
+        { target = self; }
+
+            navigator.SetDestination(target.position);
+
+
+
+
+    }
+}
