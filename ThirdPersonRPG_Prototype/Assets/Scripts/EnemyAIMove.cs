480a28156fa35f8b56965bb3b73e95bad40b9dd2
Mostafa Haque <93haque@gmail.com>
2017-08-07 15:09:20 -0400

    Find closest player in SinglePlayer

--- ThirdPersonRPG_Prototype/Assets/Scripts/EnemyAIMove.cs
+++ ThirdPersonRPG_Prototype/Assets/Scripts/EnemyAIMove.cs
@@ -26,12 +26,33 @@ public class EnemyAIMove : MonoBehaviour {
 
         }
         else
-        { target = self; }
+        { target = FindClosestEnemy().transform; }
 
             navigator.SetDestination(target.position);
 
 
 
 
+    }
+
+
+    public GameObject FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("SinglePlayer");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
     }
 }
