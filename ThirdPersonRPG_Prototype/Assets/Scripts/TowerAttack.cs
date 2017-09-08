1ff5f0c0217c3fd319caeadd5706cfd74ee5fcad
Unknown <gordon.lee.6170@gmail.com>
2017-09-08 15:23:45 -0400

    script on tower shooting

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/TowerAttack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAttack : MonoBehaviour {
+    [SerializeField]
+    private Transform firePoint;
+    [SerializeField]
+    private List<MultiplayerPlayerManager> players;
+    [SerializeField]
+    private float attackRange = 18;
+    private SphereCollider trigger;
+
+    private const string PLAYER_PREFIX = "Player";
+
+    private void Awake() {
+        players = new List<MultiplayerPlayerManager>();
+        trigger = this.GetComponent<SphereCollider>();
+        trigger.radius = attackRange;
+        if (!trigger.isTrigger) {
+            trigger.isTrigger = true;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < players.Count; i++) {
+            Debug.Log(players[i].name);
+        }
+	}
+
+    private void OnTriggerEnter(Collider _col) {
+        if (_col.tag == PLAYER_PREFIX) {
+            MultiplayerPlayerManager _player = _col.GetComponent<MultiplayerPlayerManager>();
+            players.Add(_player);
+        }
+    }
+
+    private void OnTriggerExit(Collider _col) {
+        if (_col.tag == PLAYER_PREFIX) {
+            MultiplayerPlayerManager _player = _col.GetComponent<MultiplayerPlayerManager>();
+            players.Remove(_player);
+        }
+    }
+}
