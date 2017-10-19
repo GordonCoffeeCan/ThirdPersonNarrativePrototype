f178ebeff3c980c71835469581624c3aa84b17fa
Mostafa Haque <93haque@gmail.com>
2017-10-19 18:27:44 -0400

    Animator structure changes

--- ThirdPersonRPG_Prototype/Assets/Scripts/PlayerAnimation.cs
+++ ThirdPersonRPG_Prototype/Assets/Scripts/PlayerAnimation.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour {
 
-    [SerializeField] private Animator playerAnimator;
+    [SerializeField]
+    private Animator playerAnimator;
+
+    [SerializeField]
+    private Transform leftLocator;
+    [SerializeField]
+    private Transform rightLocator;
 
     private CharacterController characterCtr;
     private PlayerController playerController;
@@ -12,7 +18,7 @@ public class PlayerAnimation : MonoBehaviour {
     private Vector2 idleBlend = Vector2.zero;
     private Vector2 currentIdleBlend = Vector2.zero;
     private Vector2 horizontalSpeed = Vector2.zero;
-    
+
     private int idleState = Animator.StringToHash("Base Layer.Human_Idle");
     private int joggingState = Animator.StringToHash("Base Layer.Jogging");
     private int runToStopState = Animator.StringToHash("Base Layer.RunToStop");
@@ -22,11 +28,13 @@ public class PlayerAnimation : MonoBehaviour {
 
     private AnimatorStateInfo currentAnimatorState;
 
-    [SerializeField]private float idleChangeTimer = 3;
+    [SerializeField]
+    private float idleChangeTimer = 3;
     private float currentIdleChangeTimer;
     private float currentLocomotionSpeed = 0;
 
-    [HideInInspector] public bool isHardLanding = false;
+    [HideInInspector]
+    public bool isHardLanding = false;
 
     private void Awake() {
         characterCtr = this.GetComponent<CharacterController>();
@@ -34,17 +42,17 @@ public class PlayerAnimation : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Start () {
-		if(playerAnimator == null) {
+    void Start() {
+        if (playerAnimator == null) {
             Debug.LogError("No player animator reference!");
             return;
         }
 
         currentIdleChangeTimer = idleChangeTimer;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         ChangeIdleBlend();
         ChangeMotion();
         currentAnimatorState = playerAnimator.GetCurrentAnimatorStateInfo(0);
@@ -52,7 +60,7 @@ public class PlayerAnimation : MonoBehaviour {
 
     private void ChangeIdleBlend() {
         currentIdleChangeTimer -= Time.deltaTime;
-        if(currentIdleChangeTimer <= 0) {
+        if (currentIdleChangeTimer <= 0) {
             idleBlend = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
             currentIdleChangeTimer = idleChangeTimer;
         }
@@ -75,33 +83,36 @@ public class PlayerAnimation : MonoBehaviour {
         playerAnimator.SetBool("Gliding", playerController.isGlide);
         playerAnimator.SetBool("HardLanding", isHardLanding);
 
-        if(currentAnimatorState.fullPathHash == runToStopState || currentAnimatorState.fullPathHash == hardLandingState) {
+        if (currentAnimatorState.fullPathHash == runToStopState || currentAnimatorState.fullPathHash == hardLandingState) {
             playerController.isAbleToMove = false;
-        } else {
+        }
+        else {
             playerController.isAbleToMove = true;
         }
 
-        if(currentAnimatorState.fullPathHash != dashingState && !playerAnimator.IsInTransition(0)) {
+        if (currentAnimatorState.fullPathHash != dashingState && !playerAnimator.IsInTransition(0)) {
             playerController.isAbleToDash = true;
-        } else {
+        }
+        else {
             playerController.isAbleToDash = false;
         }
 
         //Only idle state will trigger jump action when press jump button;
-        if((currentAnimatorState.fullPathHash == idleState || currentAnimatorState.fullPathHash == joggingState)) {
-            
+        if ((currentAnimatorState.fullPathHash == idleState || currentAnimatorState.fullPathHash == joggingState)) {
+
         }
         //Only idle state will trigger jump action when press jump button;
 
         if (!playerAnimator.IsInTransition(0) && currentAnimatorState.fullPathHash != fallingToRunningState) {
             if (MobileInputManager.instance.isGamepadConnected == false) {
 
-            } else {
+            }
+            else {
                 playerAnimator.SetBool("Jump", ControllerManager.instance.OnJump());
                 playerController.toggleJump = ControllerManager.instance.OnJump();
             }
         }
-        
+
 
         //Play Dash animation only player animator is in Jogging state and triggered dashing in PlayerController;
         if (currentAnimatorState.fullPathHash == joggingState && !playerAnimator.IsInTransition(0)) {
@@ -111,4 +122,18 @@ public class PlayerAnimation : MonoBehaviour {
         }
         //Play Dash animation only player animator is in Jogging state and triggered dashing in PlayerController;
     }
+
+   /*private float LeftToeAndRightToeOffset() {
+        float _idle2Left = 0;
+
+        if (leftLocator != null && rightLocator != null) {
+
+            _idle2Left = Vector3.Angle((rightLocator.position - leftLocator.position), this.transform.forward);
+
+        }
+        else {
+            Debug.LogError("No left or right locator reference!");
+        }
+        return _idle2Left;
+    }*/
 }
