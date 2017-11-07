f213503d32f18c52cd71b216cd221c53aa39e33a
Mostafa Haque <93haque@gmail.com>
2017-11-07 15:30:32 -0500

    construction system

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MultiplayerScripts/Mostafa's Scripts/MultiplayerConstruction.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MultiplayerConstruction : NetworkBehaviour {
+
+    public GameObject[] scaffolds;
+    public float constructionMaterialsCollected;
+
+    // Use this for initialization
+    void Start() {
+
+        for(int i = 0; i < scaffolds.Length; i++) {
+
+            Color transparentColor = scaffolds[i].GetComponent<Renderer>().material.color;
+
+            transparentColor.a = 0;
+
+            scaffolds[i].GetComponent<Renderer>().material.color = transparentColor;
+            scaffolds[i].GetComponent<Collider>().enabled = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter(Collider _col) {
+        if (_col.tag == "Player") {
+            string _playerName = _col.transform.name;
+            MultiplayerPlayerManager _player = MultiplayerGameManager.GetPlayer(_playerName);
+            if (_player.hasCargo == true) {
+                CmdDestroyCargo(_playerName);
+                constructionMaterialsCollected++;
+                for (int i = 0; i < constructionMaterialsCollected; i++) {
+
+                    Color transparentColor = scaffolds[i].GetComponent<Renderer>().material.color;
+
+                    transparentColor.a = 256;
+
+                    scaffolds[i].GetComponent<Renderer>().material.color = transparentColor;
+                     scaffolds[i].GetComponent<Renderer>().material.color = transparentColor;
+                    scaffolds[i].GetComponent<Collider>().enabled = true;
+                }
+
+            }
+        }
+    }
+
+    [Command]
+    private void CmdDestroyCargo(string _playerName) {
+        MultiplayerPlayerManager _player = MultiplayerGameManager.GetPlayer(_playerName);
+        RpcStealCargo();
+        _player.RpcDestroyCargo();
+
+    }
+
+    [ClientRpc]
+    void RpcStealCargo() {
+        MultiplayerGameManager.instance.cargoesStolen++;
+    }
+}
