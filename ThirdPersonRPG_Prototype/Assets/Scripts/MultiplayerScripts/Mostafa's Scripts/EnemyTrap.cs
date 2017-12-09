08c8b49b9ffb7c0fefabff96056be3c0619a13a6
Mostafa Haque <93haque@gmail.com>
2017-12-09 12:16:58 -0500

    Trap building

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MultiplayerScripts/Mostafa's Scripts/EnemyTrap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrap : MonoBehaviour {
+    private Animator doorAnimator;
+
+    // Use this for initialization
+    void Start () {
+        doorAnimator = this.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider _col) {
+        if (_col.tag == "Player") {
+           
+                doorAnimator.SetBool("OpenDoor", true);
+            
+        }
+    }
+
+
+}
