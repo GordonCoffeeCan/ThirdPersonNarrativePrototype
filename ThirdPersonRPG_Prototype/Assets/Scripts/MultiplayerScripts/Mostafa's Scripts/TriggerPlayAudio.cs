9b21322a845930de44c96a3cbd0f656957183c19
Mostafa Haque <93haque@gmail.com>
2017-12-14 13:12:01 -0500

    Audio Assets

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MultiplayerScripts/Mostafa's Scripts/TriggerPlayAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlayAudio : MonoBehaviour {
+
+    private bool hasTriggered = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider _col) {
+        if (hasTriggered == false) {
+            if (_col.tag == "Player") {
+
+                AudioSource audio = GetComponent<AudioSource>();
+                audio.Play();
+                hasTriggered = true;
+            }
+        }
+    }
+}
