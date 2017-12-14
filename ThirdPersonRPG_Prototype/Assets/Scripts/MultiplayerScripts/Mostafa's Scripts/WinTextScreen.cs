b6369b9476fb4e7621254a0e438e4da8308d0d3f
Mostafa Haque <93haque@gmail.com>
2017-12-14 16:56:41 -0500

    built for demo day

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MultiplayerScripts/Mostafa's Scripts/WinTextScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTextScreen : MonoBehaviour {
+
+    public GameObject winText;
+
+    public GameObject zig1;
+    public GameObject zig2;
+    public GameObject zig3;
+    public GameObject zig4;
+
+	// Use this for initialization
+	void Start () {
+
+        if (zig1.activeInHierarchy) {
+            if (zig2.activeInHierarchy) {
+                if (zig3.activeInHierarchy) {
+                    if (zig4.activeInHierarchy) {
+                        winText.SetActive(true);
+                    }
+                }
+            }
+
+        }
+        else {
+            winText.SetActive(false);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
