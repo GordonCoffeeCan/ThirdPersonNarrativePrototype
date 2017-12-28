369710af3555a31d734ef32e13ad4ddc18544b5f
Mostafa Haque <93haque@gmail.com>
2017-12-27 19:14:21 -0500

    Billboard Script

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MultiplayerScripts/Mostafa's Scripts/bibloardCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bibloardCamera : MonoBehaviour {
+    private Camera playerCam;
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        /*Transform target = Camera.main.transform;
+        if (target != null)
+            transform.rotation = target.rotation;*/
+
+        // transform.LookAt(Camera.main.transform);
+
+        if (playerCam == null) {
+            playerCam = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+        }
+        transform.LookAt(playerCam.transform);
+
+    }
+}
